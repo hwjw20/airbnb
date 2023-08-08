bb2e70c30121b4f095a07f224c8044bcedf53693
hwjw20 <hwjw666@naver.com>
2023-08-08 13:05:17 +0900

    [황지원] - footer 수정 - reservation 테이블의 userId -> guestId 컬럼명 변경

--- src/main/java/com/jiwon/airbnb/room/HostRestController.java
+++ src/main/java/com/jiwon/airbnb/room/HostRestController.java
@@ -1,7 +1,48 @@
 package com.jiwon.airbnb.room;
 
-public class HostRestController {
+import java.util.ArrayList;
+import java.util.List;
 
-	@GetMapping("/host/reservation")
-	public reservation()
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jiwon.airbnb.reservation.bo.ReservationBO;
+import com.jiwon.airbnb.reservation.model.ReservationInfo;
+import com.jiwon.airbnb.room.model.Calendar;
+
+@RestController
+@RequestMapping("/host/reservation")
+public class HostRestController {
+	
+	@Autowired
+	private ReservationBO reservationBO;
+
+	@GetMapping("/getSchedule")
+	public List<Calendar> roomSchedule(HttpSession session) {
+		
+		int userId = (int) session.getAttribute("userId");
+		
+		List<ReservationInfo> reservList = reservationBO.getReservationList(userId);
+		List<Calendar> list = new ArrayList<>();
+		
+		for(ReservationInfo reserv:reservList) {
+			
+			String title = reserv.getUserName();
+			String content = title + " 외 " + (reserv.getHeadcount() - 1) + "명";
+			
+			Calendar calendar = new Calendar();
+			calendar.setTitle(title);
+			calendar.setContent(content);
+			calendar.setStart(reserv.getDate());
+			calendar.setEnd(reserv.getEndDate());
+			
+			list.add(calendar);
+		}
+		
+		return list;
+	}
 }
